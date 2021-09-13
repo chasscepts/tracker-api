43275efab12de466287813503e49d2802db25a47
Obetta <francischassobetta@gmail.com>
2021-09-13 22:22:44 +0100

    Implement Groups controller

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,37 @@
+class GroupsController < ApplicationController
+  before_action :set_group, only: [:show, :update, :destroy]
+
+  def index
+    @groups = Group.all
+    json_response(@groups)
+  end
+
+  def show
+    json_response(@group)
+  end
+
+  def create
+    @group = Group.create!(group_params)
+    json_response(@group, :created)
+  end
+
+  def update
+    @group.update(group_params)
+    head :no_content
+  end
+
+  def destroy
+    @group.destroy
+    head :no_content
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(%i[:title])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+end
